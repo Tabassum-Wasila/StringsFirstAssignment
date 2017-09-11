b5ae59cf3c3f1615b3d4a3b4f69bb383de4d5c07
Tabassum-Wasila <twahsila@gmail.com>
2017-09-12 00:56:22 +0600

    testSimpleGene added which will test findSimpleGene method by providing copuble of dna sequences

--- Part1.java
+++ Part1.java
@@ -27,4 +27,31 @@ public class Part1{
             
             return result;
      }
+     public void testSimpleGene()
+     {
+         //No start codon
+         String dna = "ATTCAGTGCTAATGC";
+         String gene = findSimpleGene(dna);
+         System.out.println("\nDNA Strand: " + dna + "\nGene: " + gene);
+         
+         //No stop codon
+         dna = "ATGCAGTGCTACTGC";
+         gene = findSimpleGene(dna);
+         System.out.println("\nDNA Strand: " + dna + "\nGene: " + gene);
+         
+         //Distance between start and stop codon is not divisible by 3
+         dna = "ATGCAGTGCTTAATGC";
+         gene = findSimpleGene(dna);
+         System.out.println("\nDNA Strand: " + dna + "\nGene: " + gene);
+         
+         //Distance between start and stop codon is divisible by 3
+         dna = "ATGCAGTGCTAATCG";
+         gene = findSimpleGene(dna);
+         System.out.println("\nDNA Strand: " + dna + "\nGene: " + gene);
+         
+         //One start codon and two stop codons
+         dna = "ATGCAGTGCTAATAA";
+         gene = findSimpleGene(dna);
+         System.out.println("\nDNA Strand: " + dna + "\nGene: " + gene);
+     }
 }
