6fe065cdeaaec1e241dcb195355d127eab3c07b9
Tabassum-Wasila <twahsila@gmail.com>
2017-09-16 02:38:45 +0600

    testing method added which tests twoOccurrences by providing the parameters and printing the results.

--- Part3.java
+++ Part3.java
@@ -17,4 +17,39 @@ public class Part3
         
         return false;
     }
+    public void testing()
+    {
+        System.out.println("Stringa = by \nStringb = A story by Abby Long");
+        boolean check = twoOccurrences("by", "A story by Abby Long");
+        if(check)
+        {
+            System.out.println("twoOccurrences return value = true");
+        }
+        else
+        {
+            System.out.println("twoOccurrences return value = false");
+        }
+        
+        System.out.println("Stringa = a \nStringb = banana");
+        check = twoOccurrences("a", "banana");
+        if(check)
+        {
+            System.out.println("twoOccurrences return value = true");
+        }
+        else
+        {
+            System.out.println("twoOccurrences return value = false");
+        }
+        
+        System.out.println("Stringa = atg \nStringb = ctgtatgta");
+        check = twoOccurrences("atg", "ctgtatgta");
+        if(check)
+        {
+            System.out.println("twoOccurrences return value = true");
+        }
+        else
+        {
+            System.out.println("twoOccurrences return value = false");
+        }
+    }
 }
